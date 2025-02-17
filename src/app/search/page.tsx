c5273fec1e3a0dab98721648ea19dd41ff17a635
mamadfar <m.sabah.farhadi@gmail.com>
2025-02-18 00:49:01 +0330

    #feat: add drizzle-orm and drizzle-kit + connect to neon

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import {redirect} from "next/navigation";
+import {db} from "@/db";
+import {productsTable} from "@/db/schema";
+import {sql} from "drizzle-orm";
 
-const Search = () => {
+interface PageProps {
+    searchParams: {
+        [key: string]: string | string[] | undefined;
+    }
+}
+
+const Search = async ({searchParams}: PageProps) => {
+
+    const {q: query} = await searchParams
+
+    if (Array.isArray(query) || !query) return redirect('/')
+
+    let products = await db.select().from(productsTable).where(
+        sql`to_tsvector('simple', lower(${productsTable.name} || ' ' ||
+${productsTable.description})) @@ to_tsquery('simple', lower(${query.trim().split(' ').join(' & ')}))`
+    ).limit(3)
+
     return (
-        <div>
-            search
-        </div>
+        <pre>
+            {JSON.stringify(products)}
+        </pre>
     );
 };
 
